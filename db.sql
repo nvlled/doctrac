20e30defcebec9e35c9be2fe3ded7339f248a296
Ronald Casili <nvlled@gmail.com>
2018-02-20 11:48:03 +0800

    Edit sample db.sql

--- db.sql
+++ db.sql
@@ -7,43 +7,61 @@ delete from users;
 delete from documents;
 delete from document_routes;
 
+set @t = NOW();
+
 insert into campuses(id, code, name, created_at, updated_at) values
-(1,  "ala", "Alaminos", NOW(), NOW()),
-(2,  "asi", "Asingan", NOW(), NOW()),
-(3,  "bay", "Bayambang", NOW(), NOW()),
-(4,  "bin", "Binmaley", NOW(), NOW()),
-(5,  "inf", "Infanta", NOW(), NOW()),
-(6,  "lin", "Lingayen", NOW(), NOW()),
-(7,  "car", "San Carlos", NOW(), NOW()),
-(8,  "sta", "Sta Maria", NOW(), NOW()),
-(9, "urd", "urdaneta", NOW(), NOW());
+(1,  "ala", "Alaminos", @t, @t),
+(2,  "asi", "Asingan", @t, @t),
+(3,  "bay", "Bayambang", @t, @t),
+(4,  "bin", "Binmaley", @t, @t),
+(5,  "inf", "Infanta", @t, @t),
+(6,  "lin", "Lingayen", @t, @t),
+(7,  "car", "San Carlos", @t, @t),
+(8,  "sta", "Sta Maria", @t, @t),
+(9, "urd",  "Urdaneta", @t, @t);
 
 insert into offices(id, campusId, name, created_at, updated_at) values
-(@urec:=1, 9, 'Records', NOW(), NOW()),
-(@umis:=2, 9, 'MIS', NOW(), NOW()),
-(@uacc:=3, 9, 'Accounting', NOW(), NOW()),
-(@ucoc:=4, 9, 'COC', NOW(), NOW()),
-(@ucsh:=5, 9, 'Cashier', NOW(), NOW()),
-(6,        6, 'MIS', NOW(), NOW()),
-(7,        6, 'Accounting', NOW(), NOW()),
-(8,        6, 'COC', NOW(), NOW()),
-(9,        2, 'Registrar', NOW(), NOW()),
-(10,       2, 'MIS', NOW(), NOW()),
-(11,       2, 'Accounting', NOW(), NOW()),
-(12,       2, 'COC', NOW(), NOW());
+(1,        1, 'Records', @t, @t),
+(2,        1, 'MIS', @t, @t),
+(3,        1, 'Registrar', @t, @t),
+(4,        2, 'Records', @t, @t),
+(5,        2, 'MIS', @t, @t),
+(6,        2, 'Registrar', @t, @t),
+(7,        3, 'Records', @t, @t),
+(8,        3, 'MIS', @t, @t),
+(9,        3, 'Registrar', @t, @t),
+(10,       4, 'Records', @t, @t),
+(11,       4, 'MIS', @t, @t),
+(12,       4, 'Registrar', @t, @t),
+(13,       5, 'Records', @t, @t),
+(14,       5, 'MIS', @t, @t),
+(15,       5, 'Registrar', @t, @t),
+(16,       6, 'Records', @t, @t),
+(17,       6, 'MIS', @t, @t),
+(18,       6, 'Registrar', @t, @t),
+(19,       7, 'Records', @t, @t),
+(20,       7, 'MIS', @t, @t),
+(21,       7, 'Registrar', @t, @t),
+(22,       8, 'Records', @t, @t),
+(23,       8, 'MIS', @t, @t),
+(24,       8, 'Registrar', @t, @t),
+(@urec:=25,9, 'Records', @t, @t),
+(@umis:=26,9, 'MIS', @t, @t),
+(@uacc:=27,9, 'Accounting', @t, @t),
+(@ucoc:=28,9, 'COC', @t, @t),
+(@ucsh:=29,9, 'Cashier', @t, @t);
 
 insert into positions(id, name, created_at, updated_at) values
-(@head := 1,  'Head', NOW(), NOW()),
-(@asst := 2,  'Assistant', NOW(), NOW()),
-(@fact := 3,  'Faculty', NOW(), NOW()),
-(@clrk := 4,  'Clerk', NOW(), NOW());
+(@head := 1,  'Head', @t, @t),
+(@asst := 2,  'Assistant', @t, @t),
+(@fact := 3,  'Faculty', @t, @t),
+(@clrk := 4,  'Clerk', @t, @t);
 
 insert into privileges(id, name, created_at, updated_at) values
-(@admin   := 1,  'admin', NOW(), NOW()),
-(@officer := 2,  'record officer', NOW(), NOW()),
-(@agent   := 3,  'agent', NOW(), NOW());
+(@admin   := 1,  'admin', @t, @t),
+(@officer := 2,  'record officer', @t, @t),
+(@agent   := 3,  'agent', @t, @t);
 
-set @t = NOW();
 
 -- password == bcrypt('x')
 set @p = "$2y$10$eW9b3pHETP.xhdww1nUare66H39WqlQW6rLS8gGvH7OK3IN6ji66.";
@@ -58,14 +76,14 @@ insert into users
     officeId
 ) values
 (@t, @t, 1, "ala-rec@psu.edu.ph", @p, @officer, 1),
-(@t, @t, 2, "asi-rec@psu.edu.ph", @p, @officer, 2),
-(@t, @t, 3, "bay-rec@psu.edu.ph", @p, @officer, 3),
-(@t, @t, 4, "bin-rec@psu.edu.ph", @p, @officer, 4),
-(@t, @t, 5, "inf-rec@psu.edu.ph", @p, @officer, 5),
-(@t, @t, 6, "lin-rec@psu.edu.ph", @p, @officer, 6),
-(@t, @t, 7, "car-rec@psu.edu.ph", @p, @officer, 7),
-(@t, @t, 8, "sta-rec@psu.edu.ph", @p, @officer, 8),
-(@t, @t, 9, "urd-rec@psu.edu.ph", @p, @officer, 9);
+(@t, @t, 2, "asi-rec@psu.edu.ph", @p, @officer, 4),
+(@t, @t, 3, "bay-rec@psu.edu.ph", @p, @officer, 7),
+(@t, @t, 4, "bin-rec@psu.edu.ph", @p, @officer, 10),
+(@t, @t, 5, "inf-rec@psu.edu.ph", @p, @officer, 13),
+(@t, @t, 6, "lin-rec@psu.edu.ph", @p, @officer, 16),
+(@t, @t, 7, "car-rec@psu.edu.ph", @p, @officer, 19),
+(@t, @t, 8, "sta-rec@psu.edu.ph", @p, @officer, 22),
+(@t, @t, 9, "urd-rec@psu.edu.ph", @p, @officer, 25);
 
 insert into documents
 (
