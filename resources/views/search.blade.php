82600dfb21e9c4b6dc5ad706b9b098d223baab03
Ronald Casili <nvlled@gmail.com>
2018-03-05 01:07:24 -0500

    Limit search retries

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -8,11 +8,11 @@
     <fieldset>
         <div class="pure-control-group">
             <label for="name">Tracking Number</label>
-            <input id="trackingId" name="trackingId" required type="text" placeholder="e.g. campus-2018-0123" value="{{$trackingId ?? ''}}">
+            <input id="trackingId" size="30" name="trackingId" required type="text" placeholder="e.g. campus-2018-0123" value="{{$trackingId ?? ''}}">
             <span class="pure-form-message-inline"></span>
+            <button type="submit" class="pure-button pure-button-primary">Search</button>
         </div>
         <div class="pure-controls">
-            <button type="submit" class="pure-button pure-button-primary">Search</button>
             <span class="pure-form-message-inline error">{{$message ?? ""}}</span>
         </div>
     </fieldset>
