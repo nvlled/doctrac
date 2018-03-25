ffeee686ed48de7278fd3cef2fe2d5c6797d5e3c
Ronald Casili <nvlled@gmail.com>
2018-03-25 21:29:24 +0800

    Add feature tests

--- tests/Feature/RoutingTest.php
+++ tests/Feature/RoutingTest.php
@@ -26,6 +26,362 @@ class RoutingTest extends TestCase
         $this->assertTrue(true);
     }
 
+    public function testParallel() {
+        self::createUserOffices();
+
+        $officeXA = Office::withUserName("X-A");
+        $officeXB = Office::withUserName("X-B");
+        $officeXC = Office::withUserName("X-C");
+
+        $officeYA = Office::withUserName("Y-A");
+        $officeYB = Office::withUserName("Y-B");
+        $officeYC = Office::withUserName("Y-C");
+        $officeYD = Office::withUserName("Y-D");
+
+        $officeZA = Office::withUserName("Z-A");
+        $officeZB = Office::withUserName("Z-B");
+        $officeZC = Office::withUserName("Z-C");
+        $officeZD = Office::withUserName("Z-D");
+
+        $api = new DoctracAPI($officeXA->user);
+        $doc = $api->dispatchDocument([
+            "title"=>str_random(),
+            "officeIds"=>[
+                $officeYA->id,
+                $officeZA->id,
+            ],
+            "type"=>"parallel",
+        ]);
+
+        $this->assertEquals("delivering", $api->routeStatus($doc, "X-A"));
+        $this->assertEquals("waiting", $api->routeStatus($doc, "Y-A"));
+        $this->assertEquals("waiting", $api->routeStatus($doc, "Z-A"));
+        $this->assertFalse($api->hasErrors());
+
+        $api->receiveFromOffice($doc, "Y-A");
+        $this->assertFalse($api->hasErrors());
+        $api->receiveFromOffice($doc, "Z-A");
+        $this->assertFalse($api->hasErrors());
+
+        $this->assertEquals("done", $api->routeStatus($doc, "X-A"));
+        $this->assertEquals("processing", $api->routeStatus($doc, "Y-A"));
+        $this->assertEquals("processing", $api->routeStatus($doc, "Z-A"));
+
+
+
+        $doc = $api->dispatchDocument([
+            "title"=>str_random(),
+            "officeIds"=>[
+                $officeYA->id,
+                $officeZA->id,
+            ],
+            "type"=>"parallel",
+        ]);
+
+        $api->receiveFromOffice($doc, "Y-A");
+        //$api->forwardToOffices($doc, ["Y-B", "Y-C", "Y-D"]); TODO
+        $api->forwardDocument([
+            "document"=>$doc,
+            "office"  =>"Y-A",
+            "type"    =>"parallel",
+            "officeIds"=>[
+                $officeYB->id,
+                $officeYC->id,
+                $officeYD->id,
+            ],
+        ]);
+
+        $this->assertEquals("waiting", $api->routeStatus($doc, "Y-B"));
+        $this->assertEquals("waiting", $api->routeStatus($doc, "Y-C"));
+        $this->assertEquals("waiting", $api->routeStatus($doc, "Y-D"));
+
+        //$api->forwardToOffice($doc, "Y-B");
+        //$api->receiveFromOffice($doc, "Y-B");
+
+        $this->assertEquals("delivering", $api->routeStatus($doc, "X-A"));
+        $this->assertEquals("delivering", $api->routeStatus($doc, "Y-A"));
+        $this->assertEquals("waiting", $api->routeStatus($doc, "Z-A"));
+
+        $api->receiveFromOffice($doc, "Z-A");
+        $this->assertEquals("done", $api->routeStatus($doc, "X-A"));
+        $this->assertEquals("delivering", $api->routeStatus($doc, "Y-A"));
+        $this->assertEquals("processing", $api->routeStatus($doc, "Z-A"));
+
+        $api->receiveFromOffice($doc, "Y-B");
+        $this->assertEquals("processing", $api->routeStatus($doc, "Y-B"));
+        $this->assertEquals("waiting", $api->routeStatus($doc, "Y-C"));
+        $this->assertEquals("waiting", $api->routeStatus($doc, "Y-D"));
+
+        $api->receiveFromOffice($doc, "Y-C");
+        $api->receiveFromOffice($doc, "Y-D");
+
+        $this->assertEquals("processing", $api->routeStatus($doc, "Y-B"));
+        $this->assertEquals("processing", $api->routeStatus($doc, "Y-C"));
+        $this->assertEquals("processing", $api->routeStatus($doc, "Y-D"));
+
+        $api->forwardDocument([
+            "document"=>$doc,
+            "office"  =>"Z-A",
+            "type"    =>"parallel",
+            "officeIds"=>[
+                $officeZB->id,
+                $officeZC->id,
+                $officeZD->id,
+            ],
+        ]);
+
+        $this->assertEquals("processing", $api->routeStatus($doc, "Y-B"));
+        $this->assertEquals("processing", $api->routeStatus($doc, "Y-C"));
+        $this->assertEquals("processing", $api->routeStatus($doc, "Y-D"));
+        $this->assertEquals("waiting", $api->routeStatus($doc, "Z-B"));
+        $this->assertEquals("waiting", $api->routeStatus($doc, "Z-C"));
+        $this->assertEquals("waiting", $api->routeStatus($doc, "Z-D"));
+
+        $api->receiveFromOffice($doc, "Z-B");
+        $api->receiveFromOffice($doc, "Z-C");
+        $api->receiveFromOffice($doc, "Z-D");
+
+        $this->assertEquals("processing", $api->routeStatus($doc, "Y-B"));
+        $this->assertEquals("processing", $api->routeStatus($doc, "Y-C"));
+        $this->assertEquals("processing", $api->routeStatus($doc, "Y-D"));
+        $this->assertEquals("processing", $api->routeStatus($doc, "Z-B"));
+        $this->assertEquals("processing", $api->routeStatus($doc, "Z-C"));
+        $this->assertEquals("processing", $api->routeStatus($doc, "Z-D"));
+
+        $api->dumpTree($doc);
+
+    }
+
+    public function testFinalize1() {
+        self::createUserOffices();
+
+        $officeXA = Office::withUserName("X-A");
+        $officeXB = Office::withUserName("X-B");
+        $officeXC = Office::withUserName("X-C");
+
+        $officeYA = Office::withUserName("Y-A");
+        $officeYB = Office::withUserName("Y-B");
+        $officeYC = Office::withUserName("Y-C");
+
+        $officeZA = Office::withUserName("Z-A");
+        $officeZB = Office::withUserName("Z-B");
+        $officeZC = Office::withUserName("Z-C");
+
+        $api = new DoctracAPI($officeXA->user);
+        $doc = $api->dispatchDocument([
+            "title"=>str_random(),
+            "officeIds"=>[$officeXB->id, $officeXC->id],
+            "type"=>"serial",
+        ]);
+        $api->receiveFromOffice($doc, "X-B");
+        $api->forwardToOffice($doc, "X-C");
+        $api->receiveFromOffice($doc, "X-C");
+        $api->finalizeByOffice($doc, "X-C");
+        $api->dumpErrors();
+        $this->assertTrue($api->hasErrors());
+
+        $api->clearErrors();
+        $api->forwardToOffice($doc, "X-A");
+        $api->receiveFromOffice($doc, "X-A");
+        $api->finalizeByOffice($doc, "X-A");
+        $api->dumpErrors();
+        $this->assertFalse($api->hasErrors());
+
+        $this->assertEquals("done", $api->routeStatus($doc, "X-A"));
+        $this->assertEquals("done", $api->routeStatus($doc, "X-B"));
+        $this->assertEquals("done", $api->routeStatus($doc, "X-C"));
+
+        $api->forwardToOffice($doc, "X-B");
+        $api->dumpErrors();
+        $this->assertTrue($api->hasErrors());
+
+        dump($api->getTree($doc));
+    }
+
+    public function testRejection1() {
+        self::createUserOffices();
+
+        $officeXA = Office::withUserName("X-A");
+        $officeXB = Office::withUserName("X-B");
+        $officeXC = Office::withUserName("X-C");
+
+        $officeYA = Office::withUserName("Y-A");
+        $officeYB = Office::withUserName("Y-B");
+        $officeYC = Office::withUserName("Y-C");
+
+        $officeZA = Office::withUserName("Z-A");
+        $officeZB = Office::withUserName("Z-B");
+        $officeZC = Office::withUserName("Z-C");
+
+
+        $api = new DoctracAPI($officeXA->user);
+        $doc = $api->dispatchDocument([
+            "title"=>str_random(),
+            "officeIds"=>[$officeXB->id, $officeXC->id],
+            "type"=>"serial",
+        ]);
+        $this->assertFalse($api->hasErrors());
+        $routeXB = $api->followMainRoute($doc)[1];
+
+        $route = $api->receiveFromOffice($doc, "X-B");
+        $this->assertFalse($api->hasErrors());
+        $this->assertEquals("done", $api->routeStatus($doc, "X-A"));
+        $this->assertEquals("processing", $api->routeStatus($doc, "X-B"));
+
+        $route = $api->forwardToOffice($doc, "X-C");
+        $this->assertFalse($api->hasErrors());
+        $this->assertEquals("done", $api->routeStatus($doc, "X-A"));
+        $this->assertEquals("delivering", $api->routeStatus($doc, "X-B"));
+        $this->assertEquals("waiting", $api->routeStatus($doc, "X-C"));
+
+        $route = $api->receiveFromOffice($doc, "X-C");
+        $this->assertFalse($api->hasErrors());
+        $this->assertEquals("done", $api->routeStatus($doc, "X-A"));
+        $this->assertEquals("done", $api->routeStatus($doc, "X-B"));
+        $this->assertEquals("processing", $api->routeStatus($doc, "X-C"));
+
+        $route = $api->forwardToOffice($doc, "X-B");
+        $this->assertEquals("done", $api->routeStatus($doc, "X-A"));
+        $this->assertEquals("delivering", $api->routeStatus($doc, "X-C"));
+        $this->assertEquals("waiting", $api->routeStatus($doc, "X-B"));
+
+        $api->rejectByOffice($doc, "X-B");
+        $this->assertTrue($api->hasErrors());
+
+        $api->clearErrors();
+        $route = $api->receiveFromOffice($doc, "X-B");
+        $this->assertEquals("done", $api->routeStatus($doc, "X-A"));
+        $this->assertEquals("done", $api->routeStatus($doc, "X-C"));
+        $this->assertEquals("processing", $api->routeStatus($doc, "X-B"));
+
+        $api->rejectByOffice($doc, "X-B");
+        $doc = $api->getDocument($doc->trackingId);
+        $this->assertEquals("disapproved", $doc->state);
+        //$this->assertEquals("rejected", $api->routeStatus($doc, "X-B"));
+
+        $api->clearErrors();
+        $route = $api->receiveFromOffice($doc, "X-C");
+        $this->assertTrue($api->hasErrors());
+
+        $api->clearErrors();
+        $route = $api->receiveFromOffice($doc, "X-B");
+        $this->assertTrue($api->hasErrors());
+
+        $api->clearErrors();
+        $route = $api->receiveFromOffice($doc, "X-A");
+        $this->assertFalse($api->hasErrors());
+        $this->assertEquals("done", $api->routeStatus($doc, "X-A"));
+
+        $route = $api->forwardToOffice($doc, "X-B");
+        $this->assertTrue($api->hasErrors());
+
+        dump($api->getTree($doc));
+    }
+
+    public function testRejection2() {
+        self::createUserOffices();
+
+        $officeXA = Office::withUserName("X-A");
+        $officeXB = Office::withUserName("X-B");
+        $officeXC = Office::withUserName("X-C");
+
+        $officeYA = Office::withUserName("Y-A");
+        $officeYB = Office::withUserName("Y-B");
+        $officeYC = Office::withUserName("Y-C");
+
+        $officeZA = Office::withUserName("Z-A");
+        $officeZB = Office::withUserName("Z-B");
+        $officeZC = Office::withUserName("Z-C");
+
+
+        $api = new DoctracAPI($officeXA->user);
+        $doc = $api->dispatchDocument([
+            "title"=>str_random(),
+            "officeIds"=>[$officeYA->id, $officeYB->id, $officeYA->id],
+            "type"=>"serial",
+        ]);
+        $this->assertFalse($api->hasErrors());
+        $this->assertEquals("delivering", $api->routeStatus($doc, "X-A"));
+        $this->assertEquals("waiting", $api->routeStatus($doc, "Y-A"));
+
+        $api->receiveFromOffice($doc, "Y-A");
+        $this->assertFalse($api->hasErrors());
+        $this->assertEquals("done", $api->routeStatus($doc, "X-A"));
+        $this->assertEquals("processing", $api->routeStatus($doc, "Y-A"));
+
+        $api->forwardToOffice($doc, "Z-A");
+        $this->assertFalse($api->hasErrors());
+        $this->assertEquals("delivering", $api->routeStatus($doc, "Y-A"));
+        $this->assertEquals("waiting", $api->routeStatus($doc, "Z-A"));
+
+        $api->receiveFromOffice($doc, "Z-A");
+        $this->assertFalse($api->hasErrors());
+        $this->assertEquals("done", $api->routeStatus($doc, "Y-A"));
+        $this->assertEquals("processing", $api->routeStatus($doc, "Z-A"));
+
+        $api->forwardToOffice($doc, "X-B");
+        $this->assertTrue($api->hasErrors());
+
+        $api->clearErrors();
+        $api->forwardToOffice($doc, "Z-B");
+        $this->assertFalse($api->hasErrors());
+        $this->assertEquals("done", $api->routeStatus($doc, "X-A"));
+        $this->assertEquals("done", $api->routeStatus($doc, "Y-A"));
+        $this->assertEquals("delivering", $api->routeStatus($doc, "Z-A"));
+        $this->assertEquals("waiting", $api->routeStatus($doc, "Z-B"));
+
+        $api->receiveFromOffice($doc, "Z-B");
+        $this->assertFalse($api->hasErrors());
+        $this->assertEquals("done", $api->routeStatus($doc, "X-A"));
+        $this->assertEquals("done", $api->routeStatus($doc, "Y-A"));
+        $this->assertEquals("done", $api->routeStatus($doc, "Z-A"));
+        $this->assertEquals("processing", $api->routeStatus($doc, "Z-B"));
+
+        $api->rejectByOffice($doc, "Z-B");
+
+        $this->assertFalse($api->hasErrors());
+        $this->assertEquals("*", $api->routeStatus($doc, "X-A"));
+        $this->assertEquals("done", $api->routeStatus($doc, "Y-A"));
+        $this->assertEquals("waiting", $api->routeStatus($doc, "Z-A"));
+        $this->assertEquals("delivering", $api->routeStatus($doc, "Z-B"));
+
+        $api->receiveFromOffice($doc, "Z-C");
+        $this->assertTrue($api->hasErrors());
+        $api->clearErrors();
+
+        $api->receiveFromOffice($doc, "Z-A");
+        $this->assertFalse($api->hasErrors());
+        $this->assertEquals("processing", $api->routeStatus($doc, "Z-A"));
+        $this->assertEquals("*", $api->routeStatus($doc, "X-A"));
+
+        // rejected documents should
+        // not be detoured away back to the origin
+        $api->forwardToOffice($doc, "Z-B");
+        $api->dumpErrors();
+        $this->assertTrue($api->hasErrors());
+
+        $api->clearErrors();
+        $api->forwardToOffice($doc, "X-A");
+        $this->assertFalse($api->hasErrors());
+        $this->assertEquals("delivering", $api->routeStatus($doc, "Z-A"));
+        $this->assertEquals("waiting", $api->routeStatus($doc, "X-A"));
+
+        $api->receiveFromOffice($doc, "X-A");
+        $this->assertFalse($api->hasErrors());
+        $this->assertEquals("done", $api->routeStatus($doc, "Z-A"));
+        $this->assertEquals("done", $api->routeStatus($doc, "X-A"));
+
+        $api->forwardToOffice($doc, "X-B");
+        $this->assertTrue($api->hasErrors());
+
+        $api->clearErrors();
+        $api->rejectByOffice($doc, "X-A");
+        $this->assertTrue($api->hasErrors());
+
+        $api->dumpErrors();
+        $api->dumpTree($doc);
+    }
+
     public function testSerialRoutes() {
         self::createUserOffices();
 
@@ -62,13 +418,13 @@ class RoutingTest extends TestCase
         $this->assertEquals("delivering", $origin->status);
         $this->assertEquals("waiting", $origin->nextRoute->status);
 
-        $waitingRoute = $api->findWaitingRoute($doc, $origin->nextRoute->office);
+        $waitingRoute = $api->findWaitingRoutes($doc, $origin->nextRoute->office)[0];
         $this->assertEquals($origin->nextRoute->id, optional($waitingRoute)->id);
 
         $api->clearErrors();
-        $wroute = $api->receiveDocument($origin->nextRoute->office, $doc);
+        $wroute = $api->receiveDocument($origin->nextRoute, $doc);
+        $this->assertFalse($api->hasErrors());
         $this->assertEquals($waitingRoute->id, $wroute->id);
-        //$api->setReceiver($origin->nextRoute, $origin->nextRoute->office->user);
         $nextRoute = $origin->nextRoute;
         $origin = $api->origin($doc); // get recent changes
 
@@ -179,6 +535,7 @@ class RoutingTest extends TestCase
         $this->assertEquals("processing", $route->nextRoute->status);
 
 
+        $api->clearErrors();
         $route = $api->getRoute($route->nextRoute->id);
         $dests = Office::withUserNames(["Y-B", "Y-C", "Y-D"]);
         $api->forwardDocument([
@@ -186,12 +543,96 @@ class RoutingTest extends TestCase
             "officeIds" => $dests->map(function($off) { return $off->id; }),
             "type"      => "parallel",
         ]);
+        $this->assertFalse($api->hasErrors());
+        $this->assertEquals("delivering", $route->status);
+        foreach ($route->allNextRoutes() as $nextRoute) {
+            $this->assertEquals("waiting", $nextRoute->status);
+        }
+
+        $nextRoute = $route->allNextRoutes()[1];
+        $api->receiveDocument($nextRoute);
+        $this->assertFalse($api->hasErrors());
+        $route = $api->getRoute($route->id);
+        foreach ($route->allNextRoutes() as $nr) {
+            if ($nr->id == $nextRoute->id)
+                $this->assertEquals("processing", $nr->status);
+            else
+                $this->assertEquals("waiting", $nr->status);
+        }
+        $this->assertEquals("delivering", $route->status);
+
+        $api->forwardDocument([
+            "route"     => $nextRoute,
+            "officeIds" => [$nextRoute->getRecordsOffice()->id],
+            "type"      => "serial",
+        ]);
+        $route = $api->getRoute($route->id);
+        $nextRoute = $route->allNextRoutes()[1];
+        $api->dumpErrors();
+        $this->assertFalse($api->hasErrors());
+        foreach ($route->allNextRoutes() as $nr) {
+            if ($nr->id == $nextRoute->id)
+                $this->assertEquals("delivering", $nr->status);
+            else
+                $this->assertEquals("waiting", $nr->status);
+        }
+        $this->assertEquals("delivering", $route->status);
+
+
+        foreach ($route->allNextRoutes() as $nr) {
+            if ($nr->id == $nextRoute->id)
+                continue;
+            $api->receiveDocument($nr);
+        }
+        $route = $api->getRoute($route->id);
+        $this->assertEquals("done", $route->status);
+        foreach ($route->allNextRoutes() as $nr) {
+            if ($nr->id == $nextRoute->id)
+                $this->assertEquals("delivering", $nr->status);
+            else
+                $this->assertEquals("processing", $nr->status);
+        }
+
+        $nextRoutes = $route->allNextRoutes();
+        $api->forwardDocument([
+            "route"     => $nextRoutes->last(),
+            "officeIds" => $nextRoutes->map(function($r) { return $r->officeId; }),
+            "type"      => "parallel",
+        ]);
+        $api->dumpErrors();
+        $this->assertTrue($api->hasErrors());
+
+        $api->clearErrors();
+        $api->forwardDocument([
+            "route"     => $nextRoutes->last(),
+            "officeIds" => [$nextRoutes->last()->officeId],
+            "type"      => "serial",
+        ]);
+        $api->dumpErrors();
+        $this->assertTrue($api->hasErrors());
+        $this->assertEquals("processing", $nextRoutes->last()->status);
+
+
+        $nextRoute = $nextRoutes->last();
+        $destRoutes = $nextRoutes->filter(function($r) use ($nextRoute) {
+            return $r->id != $nextRoute->id;
+        });
+
+
+        $api->clearErrors();
+        $api->forwardDocument([
+            "route"     => $nextRoutes->last(),
+            "officeIds" => $destRoutes->map(function($r) { return $r->officeId; }),
+            "type"      => "parallel",
+        ]);
+        $api->dumpErrors();
+        $this->assertFalse($api->hasErrors());
 
         dump($api->getTree($doc));
     }
 
     public function testGetters() {
-        $api = new DoctracAPI();
+        $api = new DoctracAPI(\App\User::first());
 
         $offices = self::createUserOffices();
         $office1 = $offices[0];
