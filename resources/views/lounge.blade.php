c558a4ef52d5418153577e5da4abb322a8163da9
Ronald Casili <nvlled@gmail.com>
2018-04-21 23:04:22 +0800

    Add lounge page

---
+++ resources/views/lounge.blade.php
@@ -0,0 +1,35 @@
+
+
+
+@extends("layout")
+
+@section("contents")
+<section>
+    <div class="right">
+        <a class="bracket" href="/lounge/archive">archive</a>
+    </div>
+    <div class="chat-lounge-container">
+    </div>
+    <script src='{{asset("js/view/chat-lounge.js")}}'></script>
+    <script>
+    var chatLounge
+    window.addEventListener("load", function() {
+        Promise.all([
+            api.user.self(),
+            api.lounge.messages(),
+        ]).then(function(data) {
+            var user = data[0];
+            var messages = data[1];
+            loadChat(user, messages);
+        });
+        function loadChat(user, messages) {
+            chatLounge = new ChatLounge({
+                username: (user || {}).username,
+                messages: (messages || []).reverse(),
+            });
+            chatLounge.vm.mount(document.querySelector(".chat-lounge-container"));
+        }
+    });
+    </script>
+</section>
+@endsection
